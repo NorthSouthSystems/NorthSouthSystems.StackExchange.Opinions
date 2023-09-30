c44159c65480f8bcce9700ddb32e7ba3b889070d
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2023-09-30 18:13:22 -0400

    Changed SevenZipConsole for the binaries' flattened directory in the package.

--- FOSStrich.StackExchange.Opinions/SevenZip/SevenZipConsole.cs
+++ FOSStrich.StackExchange.Opinions/SevenZip/SevenZipConsole.cs
@@ -2,6 +2,7 @@
 
 using FOSStrich.IO;
 using System.Diagnostics;
+using System.IO;
 
 // Adapted from: https://stackoverflow.com/questions/7994477/extract-7zip-in-c-sharp-code
 //               https://stackoverflow.com/a/25786871
@@ -9,9 +10,14 @@ internal static class SevenZipConsole
 {
     internal static void ExtractAllFiles(string archiveFilepath, string outputDirectory)
     {
+        string sevenZipExeFilename = "7za.exe";
+
+        if (!File.Exists(sevenZipExeFilename))
+            sevenZipExeFilename = PathX.GetFullPathRelativeToCallerFilePath(@"Binaries\7za.exe");
+
         var processStartInfo = new ProcessStartInfo()
         {
-            FileName = PathX.GetFullPathRelativeToCallerFilePath("7za.exe"),
+            FileName = sevenZipExeFilename,
 
             // x : Extract all files
             // -o : Output directory
